d0db5e71d1c133f2ade5797c26422db80443e0a3
vinody-babajob <vinody@vinodys-MacBook-Pro-2.local>
2016-08-29 10:24:26 +0530

    refactored tests

--- scenarios/progressiveJobApplication/progressiveJobApplication_test.go
+++ scenarios/progressiveJobApplication/progressiveJobApplication_test.go
@@ -11,14 +11,12 @@ import (
 	"net/http"
 
 	"encoding/json"
+
+	"fmt"
 )
 
 func TestJobApplicaion(t *testing.T) {
 	basePath := "/src/BabajobIvrTesting/scenarios/progressiveJobApplication/"
-
-	ctime := time.Now().UTC()
-
-	t.Log("Current TIme " + ctime.String())
 
 	configurationReader := utils.NewConfigurationReader(
 		os.Getenv("TELEPHONY_ENV"),
@@ -35,68 +33,105 @@ func TestJobApplicaion(t *testing.T) {
 	if verr != nil {
 		t.Log(verr.Error())
 	}
-	t.Log(vn)
 
+	toNum := vn
+	ctime := time.Now().UTC()
+	callsid := "2344"
+	callErr := MakeInBoundCall(toNum, callsid, configurationReader, t)
+	if callErr != nil {
+		t.Errorf("Failed while making inbound call %q", callErr.Error())
+	}
+
+	time.Sleep(time.Second * 10)
+
+	_, mCallErr := IsInboundCallMadeForCallSid(callsid, configurationReader, t)
+	if mCallErr != nil {
+		t.Errorf("No inbound call found because %q", mCallErr.Error())
+	}
+
+	time.Sleep(time.Second * 20)
+	_, ocallErr := CheckIfOutboundCallMade(ctime, configurationReader, t)
+	if ocallErr != nil {
+		t.Errorf("No outbound call found because %q", ocallErr.Error())
+	}
+}
+
+func CheckIfOutboundCallMade(timeCheck time.Time, configurationReader utils.ConfigurationReader, t *testing.T) (bool, error) {
+	outboundCollection := configurationReader.GetValue("outboundCallCollection")
+	mongoConf := configurationReader.GetMapValue("mongo")
+	mongoDb := utils.NewMongoDBWithConfig(mongoConf)
+
+	outboundSession, outboundCallsRepo, outboundErr := mongoDb.GetCollection(outboundCollection)
+	if outboundErr != nil {
+		defer outboundSession.Close()
+		t.Errorf("Trouble Getting repo for outboundCall with errors %q", outboundErr.Error())
+		return false, outboundErr
+	}
+	outboundCalls := make([]models.OutboundCallRequest, 0, 1)
+	selector := bson.M{
+		"$and": []interface{}{
+			bson.M{"mobilenumber": "07338466702"},
+			bson.M{"CreatedAt": bson.M{"$gte": timeCheck}},
+		},
+	}
+	err := outboundCallsRepo.Find(selector).All(&outboundCalls)
+	if err != nil {
+		defer outboundSession.Close()
+		t.Errorf("Trouble Getting the  outbound call:%q with errors %q", "07338466702", err.Error())
+		return false, err
+	}
+	defer outboundSession.Close()
+
+	if len(outboundCalls) <= 0 {
+		t.Errorf("No Entry for the  outbound call:%q", "07338466702")
+		return false, fmt.Errorf("No outbound Made for users %q", "07338466702")
+	}
+
+	return true, nil
+}
+
+func MakeInBoundCall(toNum string, callsid string, configurationReader utils.ConfigurationReader, t *testing.T) error {
 	inboundUrl := configurationReader.GetValue("inboundCallUrl")
 
 	httpProvider := utils.NewHTTPProvider(inboundUrl, map[string]string{})
 	queryParams := map[string]string{
 		"From":          "07338466702",
-		"To":            vn,
-		"CallSid":       "2344",
-		"TransactionId": "2244",
+		"To":            toNum,
+		"CallSid":       callsid,
+		"TransactionId": "2223",
 	}
 	_, err := httpProvider.Get(queryParams)
 	if err != nil {
 		t.Errorf("Error while Making inbound call with error %q", err.Error())
+		return err
 	}
 
-	time.Sleep(time.Second * 10)
+	return nil
+}
 
+func IsInboundCallMadeForCallSid(callSid string, configurationReader utils.ConfigurationReader, t *testing.T) (bool, error) {
 	mongoConf := configurationReader.GetMapValue("mongo")
 	mongoDb := utils.NewMongoDBWithConfig(mongoConf)
 
 	inboundCollection := configurationReader.GetValue("inboundCallCollection")
 	session, inboundCallsRepo, err := mongoDb.GetCollection(inboundCollection)
 	inboundCalls := make([]models.InboundCall, 0, 1)
-	selector := bson.M{"providerCallIdentifier": "2344"}
+	selector := bson.M{"providerCallIdentifier": callSid}
 	err = inboundCallsRepo.Find(selector).All(&inboundCalls)
 	if err != nil {
 		defer session.Close()
-		t.Errorf("Trouble Getting the callsid inboundcall:%q with errors %q", "2344", err.Error())
+		t.Errorf("Trouble Getting the callsid inboundcall:%q with errors %q", callSid, err.Error())
+		return false, err
 	}
 
 	defer session.Close()
 
 	if len(inboundCalls) <= 0 {
-		t.Errorf("No Entry for the callsid inboundcall:%q", "2344")
+		t.Errorf("No Entry for the callsid inboundcall:%q", callSid)
+		return false, fmt.Errorf("No InboundCalls Made for callsid %q", callSid)
 	}
 
-	time.Sleep(time.Second * 20)
-
-	outboundCollection := configurationReader.GetValue("outboundCallCollection")
-	outboundSession, outboundCallsRepo, outboundErr := mongoDb.GetCollection(outboundCollection)
-	if outboundErr != nil {
-		defer outboundSession.Close()
-		t.Errorf("Trouble Getting repo for outboundCall with errors %q", outboundErr.Error())
-	}
-	outboundCalls := make([]models.OutboundCallRequest, 0, 1)
-	selector = bson.M{
-		"$and": []interface{}{
-			bson.M{"mobilenumber": "07338466702"},
-			bson.M{"CreatedAt": bson.M{"$gte": ctime}},
-		},
-	}
-	err = outboundCallsRepo.Find(selector).All(&outboundCalls)
-	if err != nil {
-		defer outboundSession.Close()
-		t.Errorf("Trouble Getting the  outbound call:%q with errors %q", "07338466702", err.Error())
-	}
-	defer outboundSession.Close()
-
-	if len(outboundCalls) <= 0 {
-		t.Errorf("No Entry for the  outbound call:%q", "07338466702")
-	}
+	return true, nil
 }
 
 func GetVirtualNumber(userId string, configReader utils.ConfigurationReader, t *testing.T) (string, error) {
@@ -106,7 +141,7 @@ func GetVirtualNumber(userId string, configReader utils.ConfigurationReader, t *
 		Id                int
 	}
 
-	type VK struct {
+	type VirtualAssoc struct {
 		Caller        User
 		Callee        User
 		VirtualNumber string `json:"virtualNumber"`
@@ -140,10 +175,13 @@ func GetVirtualNumber(userId string, configReader utils.ConfigurationReader, t *
 	}
 
 	if response.StatusCode == http.StatusOK {
-		vnum := []VK{}
+		vnum := []VirtualAssoc{}
 		t.Log(string(response.Content))
 		err = json.Unmarshal(response.Content, &vnum)
-		t.Log(vnum)
+		if err != nil {
+			return "", err
+		}
+
 		return vnum[0].VirtualNumber, nil
 	}
 
--- scenarios/progressiveRegistration/progressiveRegistration_test.go
+++ scenarios/progressiveRegistration/progressiveRegistration_test.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"fmt"
 )
 
 func TestSeekerRegistrationCall(t *testing.T) {
@@ -18,66 +20,102 @@ func TestSeekerRegistrationCall(t *testing.T) {
 	)
 
 	ctime := time.Now().UTC()
-
-	inboundUrl := configurationReader.GetValue("inboundCallUrl")
 	toNum := configurationReader.GetValue("seekerRegNumber")
+	callsid := "23e3"
+	callErr := MakeInBoundCall(toNum, callsid, configurationReader, t)
+	if callErr != nil {
+		t.Errorf("Failed while making inbound call %q", callErr.Error())
+	}
+
+	time.Sleep(time.Second * 10)
+
+	_, mCallErr := IsInboundCallMadeForCallSid(callsid, configurationReader, t)
+	if mCallErr != nil {
+		t.Errorf("No inbound call found because %q", mCallErr.Error())
+	}
+
+	time.Sleep(time.Second * 20)
+
+	_, ocallErr := CheckIfOutboundCallMade(ctime, configurationReader, t)
+	if ocallErr != nil {
+		t.Errorf("No outbound call found because %q", ocallErr.Error())
+	}
+}
+
+func CheckIfOutboundCallMade(timeCheck time.Time, configurationReader utils.ConfigurationReader, t *testing.T) (bool, error) {
+	outboundCollection := configurationReader.GetValue("outboundCallCollection")
+	mongoConf := configurationReader.GetMapValue("mongo")
+	mongoDb := utils.NewMongoDBWithConfig(mongoConf)
+
+	outboundSession, outboundCallsRepo, outboundErr := mongoDb.GetCollection(outboundCollection)
+	if outboundErr != nil {
+		defer outboundSession.Close()
+		t.Errorf("Trouble Getting repo for outboundCall with errors %q", outboundErr.Error())
+		return false, outboundErr
+	}
+	outboundCalls := make([]models.OutboundCallRequest, 0, 1)
+	selector := bson.M{
+		"$and": []interface{}{
+			bson.M{"mobilenumber": "07338466702"},
+			bson.M{"CreatedAt": bson.M{"$gte": timeCheck}},
+		},
+	}
+	err := outboundCallsRepo.Find(selector).All(&outboundCalls)
+	if err != nil {
+		defer outboundSession.Close()
+		t.Errorf("Trouble Getting the  outbound call:%q with errors %q", "07338466702", err.Error())
+		return false, err
+	}
+	defer outboundSession.Close()
+
+	if len(outboundCalls) <= 0 {
+		t.Errorf("No Entry for the  outbound call:%q", "07338466702")
+		return false, fmt.Errorf("No outbound Made for users %q", "07338466702")
+	}
+
+	return true, nil
+}
+
+func MakeInBoundCall(toNum string, callsid string, configurationReader utils.ConfigurationReader, t *testing.T) error {
+	inboundUrl := configurationReader.GetValue("inboundCallUrl")
 
 	httpProvider := utils.NewHTTPProvider(inboundUrl, map[string]string{})
 	queryParams := map[string]string{
 		"From":          "07338466702",
 		"To":            toNum,
-		"CallSid":       "23e3",
+		"CallSid":       callsid,
 		"TransactionId": "2223",
 	}
 	_, err := httpProvider.Get(queryParams)
 	if err != nil {
 		t.Errorf("Error while Making inbound call with error %q", err.Error())
+		return err
 	}
 
-	time.Sleep(time.Second * 10)
+	return nil
+}
 
+func IsInboundCallMadeForCallSid(callSid string, configurationReader utils.ConfigurationReader, t *testing.T) (bool, error) {
 	mongoConf := configurationReader.GetMapValue("mongo")
 	mongoDb := utils.NewMongoDBWithConfig(mongoConf)
 
 	inboundCollection := configurationReader.GetValue("inboundCallCollection")
 	session, inboundCallsRepo, err := mongoDb.GetCollection(inboundCollection)
 	inboundCalls := make([]models.InboundCall, 0, 1)
-	selector := bson.M{"providerCallIdentifier": "23e3"}
+	selector := bson.M{"providerCallIdentifier": callSid}
 	err = inboundCallsRepo.Find(selector).All(&inboundCalls)
 	if err != nil {
 		defer session.Close()
-		t.Errorf("Trouble Getting the callsid inboundcall:%q with errors %q", "23e3", err.Error())
+		t.Errorf("Trouble Getting the callsid inboundcall:%q with errors %q", callSid, err.Error())
+		return false, err
 	}
 
 	defer session.Close()
 
 	if len(inboundCalls) <= 0 {
-		t.Errorf("No Entry for the callsid inboundcall:%q", "23e3")
+		t.Errorf("No Entry for the callsid inboundcall:%q", callSid)
+		return false, fmt.Errorf("No InboundCalls Made for callsid %q", callSid)
 	}
 
-	time.Sleep(time.Second * 20)
-
-	outboundCollection := configurationReader.GetValue("outboundCallCollection")
-	outboundSession, outboundCallsRepo, outboundErr := mongoDb.GetCollection(outboundCollection)
-	if outboundErr != nil {
-		defer outboundSession.Close()
-		t.Errorf("Trouble Getting repo for outboundCall with errors %q", outboundErr.Error())
-	}
-	outboundCalls := make([]models.OutboundCallRequest, 0, 1)
-	selector = bson.M{
-		"$and": []interface{}{
-			bson.M{"mobilenumber": "07338466702"},
-			bson.M{"CreatedAt": bson.M{"$gte": ctime}},
-		},
-	}
-	err = outboundCallsRepo.Find(selector).All(&outboundCalls)
-	if err != nil {
-		defer outboundSession.Close()
-		t.Errorf("Trouble Getting the  outbound call:%q with errors %q", "07338466702", err.Error())
-	}
-	defer outboundSession.Close()
-
-	if len(outboundCalls) <= 0 {
-		t.Errorf("No Entry for the  outbound call:%q", "07338466702")
-	}
+	return true, nil
 }
